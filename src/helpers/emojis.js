ed706ac2a0817591a5aae98b190f3834d64fb711
Alex Chou <achou@wayfair.com>
2018-05-28 15:46:13 -0400

    Use like emoji

--- src/helpers/emojis.js
+++ src/helpers/emojis.js
@@ -3,6 +3,6 @@
 export const THINKING_EMOJI = String.fromCodePoint(0x1F914);
 export const EXPRESSIONLESS_EMOJI = String.fromCodePoint(0x1F611);
 export const ANGRY_EMOJI = String.fromCodePoint(0x1F621);
-export const SLEEPING_EMOJI = String.fromCodePoint(0x1F634);
 export const LOVE_EMOJI = String.fromCodePoint(0x1F60D);
-export const DEFAULT_EMOJIS = [THINKING_EMOJI, EXPRESSIONLESS_EMOJI, ANGRY_EMOJI, SLEEPING_EMOJI, LOVE_EMOJI];
+export const THUMBS_EMOJI = String.fromCodePoint(0x1F44D);
+export const DEFAULT_EMOJIS = [THUMBS_EMOJI, THINKING_EMOJI, EXPRESSIONLESS_EMOJI, ANGRY_EMOJI, LOVE_EMOJI];

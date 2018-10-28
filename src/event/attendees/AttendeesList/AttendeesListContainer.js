6965ce1e0f298a7a1ddd140e21c3fb1997ff0017
Alex Chou <achou@wayfair.com>
2018-10-28 16:28:39 -0400

    Change logic of timer to be purely client-side

--- src/event/attendees/AttendeesList/AttendeesListContainer.js
+++ src/event/attendees/AttendeesList/AttendeesListContainer.js
@@ -2,20 +2,16 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import AttendeesList from "./AttendeesList";
-import {
-  selectActiveEventAttendeesList,
-  selectActiveEventMyUpdatedTime
-} from "../../activeEventSelectors";
+import { selectActiveEventAttendeesList } from "../../activeEventSelectors";
 import { fetchEventUsers } from "../../eventActions";
 import { refreshMe } from "../../eventUserActions";
 
-const REFRESH_THRESHOLD_MS = 60 * 1000;
+const REFRESH_THRESHOLD_MS = 10 * 1000;
 const CHECK_FOR_REFRESH_INTERVAL_MS = 5 * 1000;
 
 class AttendeesListContainer extends Component {
   static propTypes = {
     attendees: PropTypes.array,
-    updatedTime: PropTypes.number,
     selectedAttendee: PropTypes.object, // TODO: Implement
     deselectAttendee: PropTypes.func.isRequired,
     fetchEventUsers: PropTypes.func.isRequired,
@@ -24,29 +20,35 @@ class AttendeesListContainer extends Component {
 
   static defaultProps = {
     attendees: [],
-    updatedTime: null,
     selectedAttendee: null,
     deselectAttendee() {}
   };
 
-  state = {};
+  state = {
+    startTimerTime: null
+  };
 
-  componentDidMount() {
-    this.props.fetchEventUsers();
-
-    this.timer = setInterval(() => {
-      if (this.getShouldRefresh()) {
-        this.props.refreshMe();
-      }
-    }, CHECK_FOR_REFRESH_INTERVAL_MS);
+  async componentDidMount() {
+    await this.props.fetchEventUsers();
+    this.setState({ startTimerTime: new Date().getTime() });
+    this.timer = setInterval(this.refresh, CHECK_FOR_REFRESH_INTERVAL_MS);
   }
 
   componentWillUnmount() {
     clearInterval(this.timer);
   }
 
-  getShouldRefresh = () =>
-    new Date().getTime() - this.props.updatedTime > REFRESH_THRESHOLD_MS;
+  refresh = async () => {
+    if (
+      new Date().getTime() - this.state.startTimerTime <
+      REFRESH_THRESHOLD_MS
+    ) {
+      return;
+    }
+
+    await this.props.refreshMe();
+    this.setState({ startTimerTime: new Date().getTime() });
+  };
 
   handleModalClose = () => this.props.deselectAttendee();
 
@@ -62,8 +64,7 @@ class AttendeesListContainer extends Component {
 }
 
 const mapStateToProps = state => ({
-  attendees: selectActiveEventAttendeesList(state),
-  updatedTime: selectActiveEventMyUpdatedTime(state)
+  attendees: selectActiveEventAttendeesList(state)
 });
 
 const mapDispatchToProps = {
